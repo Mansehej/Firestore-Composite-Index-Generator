4887a74284b37f678bae0ceb1166364c6f0fe164
Mansehej Singh <mansehej@gmail.com>
2021-02-01 01:30:29 +0530

    Take input and parse query details

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const { askQueryDetails } = require('./lib/questions');
+const { addBackticksToString } = require('./lib/helpers')
 
 clear();
 
@@ -9,3 +11,16 @@ console.log(
         figlet.textSync('Firestore-CIG', { horizontalLayout: 'full' })
     )
 );
+
+async function run() {
+    const queryDetails = await askQueryDetails()
+    for (var detail in queryDetails) {
+        const detailList = queryDetails[detail].split(',')
+        const whiteSpaceTrimmedList = detailList.map(element => element.trim())
+        const nonEmptyValueList = whiteSpaceTrimmedList.filter(element => element.length > 0)
+        const multipleWordParsedList = nonEmptyValueList.map(element => element.split(' ').length > 1 ? addBackticksToString(element) : element)
+        queryDetails[detail] = multipleWordParsedList
+    }
+};
+
+run();
