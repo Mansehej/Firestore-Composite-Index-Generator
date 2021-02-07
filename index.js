1c043f4a9ca6ddedf29dcb97903932edc14abc17
Mansehej Singh <mansehej@gmail.com>
2021-02-07 22:20:21 +0530

    Check if firestore index exists at given path

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const { askQueryDetails } = require('./lib/questions');
+
+const { askQueryDetails, askIndexFilePath } = require('./lib/questions');
 const { addBackticksToString } = require('./lib/helpers')
+const { getCompositeIndexes } = require('./lib/generator')
+const { indexFileExists } = require('./lib/files');
 
 clear();
 
@@ -13,7 +16,8 @@ console.log(
 );
 
 async function run() {
-    const queryDetails = await askQueryDetails()
+    var queryDetails = await askQueryDetails()
+    var filePath = await askIndexFilePath()
     for (var detail in queryDetails) {
         const detailList = queryDetails[detail].split(',')
         const whiteSpaceTrimmedList = detailList.map(element => element.trim())
@@ -21,6 +25,14 @@ async function run() {
         const multipleWordParsedList = nonEmptyValueList.map(element => element.split(' ').length > 1 ? addBackticksToString(element) : element)
         queryDetails[detail] = multipleWordParsedList
     }
+    const compositeIndexes = getCompositeIndexes(queryDetails)
+    if (indexFileExists(filePath.path)) {
+        // TODO add indexes
+    }
+    else {
+        // TODO create file and add indexes
+    }
+
 };
 
 run();
